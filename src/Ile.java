86ee202fb0dc9c060b8801369e7798d9bba2db6a
AlwaysHacked <serge.hakobyan1@gmail.com>
2022-03-28 14:59:55 +0200

    `Ile` added

---
+++ src/Ile.java
@@ -0,0 +1,10 @@
+package src;
+
+public class Ile {
+    private final int sizeGrille = 6;
+    private final int s = sizeGrille;
+    private int[][] Grille = new int[s][s];
+
+    private final int
+
+}
