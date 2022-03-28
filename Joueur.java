acc87baf27912d72e50d3d0242c1385eb3ab83b9
PierreDerathe <pierre.derathe@u-psud.fr>
2022-03-28 15:14:39 +0200

    get and set Joueur

--- Joueur.java
+++ Joueur.java
@@ -1,2 +1,27 @@
+import java.util.ArrayList;
+
 public class Joueur {
+    private Case position;
+    private ArrayList<Item> inventaire = new ArrayList<>();
+
+    public Joueur(Case position) {
+        this.position = position;
+    }
+
+
+    public Case getPosition() {
+        return position;
+    }
+
+    public void setPosition(Case position) {
+        this.position = position;
+    }
+
+    public ArrayList<Item> getInventaire() {
+        return inventaire;
+    }
+
+    public void setInventaire(ArrayList<Item> inventaire) {
+        this.inventaire = inventaire;
+    }
 }
